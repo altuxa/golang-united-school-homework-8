a21f172626f3b597250814e4bf5cc9c489921953
Altynbek <altuxadark98@gmail.com>
2022-06-13 18:15:46 +0600

    go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"os"
 )
@@ -15,4 +16,29 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func parseArgs() Arguments {
+	_ = os.Args[1:]
+	choseOperation := flag.String("operation", "", "chose operation")
+	inputBody := flag.String("item", "", "body for add")
+	choseFile := flag.String("fileName", "", "chose file")
+	flag.Parse()
+	mp := Arguments{}
+	mp["operation"] = *choseOperation
+	mp["item"] = *inputBody
+	mp["fileName"] = *choseFile
+	return mp
+}
+
+func GetInfo() {
+}
+
+func AddNewItem() {
+}
+
+func RemoveUser() {
+}
+
+func FindByID() {
 }
